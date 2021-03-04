a829025414cdeb166ad3f7b7827bfa97b0cf9ff6
Ekaterina Dolgushina <dolgushinacd@icloud.com>
2021-03-04 23:42:18 +0700

    fix images, add last block on hp

--- sites/simple-food-blog/js/homepage.js
+++ sites/simple-food-blog/js/homepage.js
@@ -1,11 +1,28 @@
 (function () {
   const blogArticle = new Swiper('.swiper-blog-article', {
-    slidesPerView: 'auto',
-    spaceBetween: 30,
     grabCursor: true,
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
-    },
+    }, breakpoints: {
+      320: {
+        sslidesPerView: 'auto',
+        centeredSlides: true,
+        spaceBetween: 30,
+      },
+      1365: {
+        slidesPerView: 'auto',
+        spaceBetween: 30,
+      }
+    }
   });
+  
+  function classToggle() {
+    const navs = document.querySelectorAll('.header__navigation-list')
+    console.log(navs);
+    navs.forEach(nav => nav.classList.toggle('header__toggle--show'));
+  }
+  
+  document.querySelector('.header__toggle')
+    .addEventListener('click', classToggle);
 })();
